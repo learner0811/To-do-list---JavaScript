5ef32e28cc1957efe68babc96397e0e069e8518c
ns_red <badboy.ndh@gmail.com>
2018-11-03 17:56:43 +0700

    version 2

--- todolistProject/todolist.js
+++ todolistProject/todolist.js
@@ -3,23 +3,58 @@ var addTaskbtn = document.getElementById("addBtn");
 var list = document.getElementById("list");
 var removeList = document.getElementsByClassName("remove");
 var checkbox = document.getElementsByClassName("cb");
+var myStorage = window.localStorage;
+
+var lisTask = [];
+
+
+
+window.onload = function(){
+	lisTask = JSON.parse(myStorage.getItem("todo"));
+	if (lisTask == null)
+		lisTask = [];
+	else
+		for (let i = 0; i < lisTask.length; i++){
+			let newItem = document.createElement("li");		
+
+			let newTask = document.createElement("span");
+			let newText = document.createTextNode(lisTask[i]);
+			newTask.appendChild(newText);		
+
+			let newcheckBox = document.createElement("input");
+			newcheckBox.setAttribute("type", "checkbox");		
+			newcheckBox.setAttribute("class", "cb");
+			newcheckBox.addEventListener("click", function(e){
+			changeCb()}, false);
+
+			let newRemove = document.createElement("span");
+			newRemove.setAttribute("class", "remove");
+			newRemove.addEventListener("click", removeFromList, false);
+
+			newItem.appendChild(newcheckBox);
+			newItem.appendChild(newTask);
+			newItem.appendChild(newRemove);		
+			
+			list.appendChild(newItem);
+		}
+}
 
 function addTask () {
-	var taskText = task.value;
+	let taskText = task.value;
 	if (taskText != ""){
-		var newItem = document.createElement("li");		
+		let newItem = document.createElement("li");		
 
-		var newTask = document.createElement("span");
-		var newText = document.createTextNode(taskText);
+		let newTask = document.createElement("span");
+		let newText = document.createTextNode(taskText);
 		newTask.appendChild(newText);		
 
-		var newcheckBox = document.createElement("input");
+		let newcheckBox = document.createElement("input");
 		newcheckBox.setAttribute("type", "checkbox");		
 		newcheckBox.setAttribute("class", "cb");
 		newcheckBox.addEventListener("click", function(e){
 		changeCb()}, false);
 
-		var newRemove = document.createElement("span");
+		let newRemove = document.createElement("span");
 		newRemove.setAttribute("class", "remove");
 		newRemove.addEventListener("click", removeFromList, false);
 
@@ -28,16 +63,27 @@ function addTask () {
 		newItem.appendChild(newRemove);		
 		
 		list.appendChild(newItem);
+						
+		lisTask.push(taskText);
+		myStorage.setItem("todo", JSON.stringify(lisTask));
 
 		task.value = "";
 	}		
 }
 
-function removeFromList () {
+function removeFromList () {		
 	var grandParent = this.parentNode.parentNode;
 	var parent = this.parentNode;	
+	var text = parent.firstChild.nextSibling.textContent;
+	
+	for (let i = 0; i < lisTask.length; i++){
+		if (lisTask[i] == text){
+			lisTask.splice(i, 1);
+			console.log('comehere');
+			myStorage.setItem("todo", JSON.stringify(lisTask));
+		}
+	}		
 	grandParent.removeChild(parent);
-
 }
 
 function changeCb (e) {
